97ee963c0b31271cf544f59ed5556a62ac78013b
mariabrock <mariabrock414@gmail.com>
2019-10-12 12:50:36 -0500

    the eat and fight boxes print to the dom

--- src/javascripts/components/eat/eat.js
+++ src/javascripts/components/eat/eat.js
@@ -2,9 +2,28 @@ import utilities from '../../helpers/utilities';
 import './eat.scss';
 
 const loadEat = () => {
-  let domString = '';
-  domString += 'eat all the things';
+  let fullness = 100;
+  let domString = '<h4>Eat</h4>';
+  domString += `<p><progress value="${fullness}" max="100" id="eatAmt"></progress></p>`;
+  domString += '<button id="carrot" type="button">Carrot<i class="fas fa-carrot fa-3x"></i></button>';
+  domString += '<button id="cookie" type="button">Cookie<i class="fas fa-cookie fa-3x"></i></button>';
+
   utilities.printToDom(domString, 'eat');
+  const carrotBtn = document.getElementById('carrot');
+  const cookieBtn = document.getElementById('cookie');
+
+  carrotBtn.addEventListener('click', () => {
+    if (fullness + 10 <= 100) {
+      fullness += 10;
+      document.getElementById('eatAmt').value = fullness;
+    }
+  });
+  cookieBtn.addEventListener('click', () => {
+    if (fullness - 3 >= 0) {
+      fullness -= 3;
+      document.getElementById('eatAmt').value = fullness;
+    }
+  });
 };
 
 export default { loadEat };
--- src/javascripts/components/fight/fight.js
+++ src/javascripts/components/fight/fight.js
@@ -1,8 +1,30 @@
 // import utilities from '../../helpers/utilities';
 import './fight.scss';
+import utilities from '../../helpers/utilities';
 
 const loadFight = () => {
-  console.error('imma fight you');
+  let strength = 100;
+  let strengthString = '<h4>Fight</h4>';
+  strengthString += `<p><progress value="${strength}" max="100" id="strengthAmt"></progress></p>`;
+  strengthString += '<button id="brave" type="button">Be Brave!<i class="fa fa-hand-spock-o" aria-hidden="true"></i></button>';
+  strengthString += '<button id="mean" type="button">Fight!<i class="fa fa-exclamation-triangle" aria-hidden="true"></i></button>';
+
+  utilities.printToDom(strengthString, 'fight');
+  const buddyBtn = document.getElementById('brave');
+  const bullyBtn = document.getElementById('mean');
+
+  buddyBtn.addEventListener('click', () => {
+    if (strength + 1 <= 100) {
+      strength += 1;
+      document.getElementById('strengthAmt').value = strength;
+    }
+  });
+  bullyBtn.addEventListener('click', () => {
+    if (strength - 10 >= 0) {
+      strength += -10;
+      document.getElementById('strengthAmt').value = strength;
+    }
+  });
 };
 
 export default { loadFight };

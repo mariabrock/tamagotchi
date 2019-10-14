a82925123cc8a4712bdf504c22586b5d12be15b8
mariabrock <mariabrock414@gmail.com>
2019-10-14 12:02:40 -0500

    styled buttons and headers

--- src/javascripts/components/fight/fight.js
+++ src/javascripts/components/fight/fight.js
@@ -1,13 +1,13 @@
-// import utilities from '../../helpers/utilities';
-import './fight.scss';
 import utilities from '../../helpers/utilities';
+import './fight.scss';
+
 
 const loadFight = () => {
   let strength = 100;
-  let strengthString = '<h4>Fight</h4>';
+  let strengthString = '<h1>Fight</h1>';
   strengthString += `<p><progress value="${strength}" max="100" id="strengthAmt"></progress></p>`;
-  strengthString += '<button id="brave" type="button">Be Brave!<i class="fa fa-hand-spock-o" aria-hidden="true"></i></button>';
-  strengthString += '<button id="mean" type="button">Fight!<i class="fa fa-exclamation-triangle" aria-hidden="true"></i></button>';
+  strengthString += '<button id="brave" type="button">Be Brave!<i class="fa fa-hand-spock-o fa-2x" aria-hidden="true"></i></button>';
+  strengthString += '<button id="mean" type="button">Fight!<i class="fa fa-exclamation-triangle fa-2x" aria-hidden="true"></i></button>';
 
   utilities.printToDom(strengthString, 'fight');
   const buddyBtn = document.getElementById('brave');
--- src/javascripts/components/sleep/sleep.js
+++ src/javascripts/components/sleep/sleep.js
@@ -2,11 +2,11 @@ import utilities from '../../helpers/utilities';
 import './sleep.scss';
 
 const loadSleep = () => {
-  let energyString = '<h4>Energy</h4>';
+  let energyString = '<h1>Energy</h1>';
   let energy = 50;
   energyString += `<p><progress value="${energy}" max="100" id="sleepAmt"></progress></p>`;
-  energyString += '<button id="nap" type="button">Nap<i class="fa fa-cat fa-3x"></i></button>';
-  energyString += '<button id="deepsleep" type="button">Sleep<i class="fa fa-bed" aria-hidden="true"></i></button>';
+  energyString += '<button id="nap" type="button">Nap<i class="fa fa-cat fa-2x"></i></button>';
+  energyString += '<button id="deepsleep" type="button">Sleep<i class="fa fa-bed fa-2x" aria-hidden="true"></i></button>';
   utilities.printToDom(energyString, 'sleep');
   const napBtn = document.getElementById('nap');
   const sleepBtn = document.getElementById('deepsleep');
